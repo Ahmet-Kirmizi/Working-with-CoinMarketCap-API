9a0dfa79d5d35834d89acc0f1f66d0348e8aa96d
Ahmet Kirmizi <62718773+ahmetk5113@users.noreply.github.com>
2021-03-27 12:42:48 +0300

    Delete trading_bot.py

--- trading_bot.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("https://www.binance.com/en/trade/DOGE_USDT")
-
-thing = driver.find_element_by_class_name("css-uliqdc")
-print(thing)
